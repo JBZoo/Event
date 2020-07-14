06df7732de3db2d27017814d9df2e42d5434b0d9
Denis Smetannikov <smet.denis@gmail.com>
2020-07-15 02:52:53 +0300

    Fixes versions

---
+++ tests/EventComposerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * JBZoo Toolbox - Event
+ *
+ * This file is part of the JBZoo Toolbox project.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package    Event
+ * @license    MIT
+ * @copyright  Copyright (C) JBZoo.com, All rights reserved.
+ * @link       https://github.com/JBZoo/Event
+ */
+
+namespace JBZoo\PHPUnit;
+
+/**
+ * Class EventComposerTest
+ *
+ * @package JBZoo\PHPUnit
+ */
+class EventComposerTest extends AbstractComposerTest
+{
+
+}
